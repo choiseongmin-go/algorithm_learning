3af3d44e89cb3d0d269dcb318dbca30bf8e3110e
ChoiSeongMin <chsm1045@naver.com>
2026-02-01 16:21:43 +0900

    asdfsdf

--- BOJ_1068.cpp
+++ BOJ_1068.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 vector<int> tree[51];
-bool visited[51];
-int answer = 0;
-int del;
+int N, del, root, answer = 0;
 
 void dfs(int cur)
 {
     if (cur == del)
         return;
-    visited[cur] = true;
-    int childCount = 0;
 
-    for (auto next : tree[cur])
+    int childCount = 0;
+    for (int next : tree[cur])
     {
-        if (visited[next])
+        if (next == del)
             continue;
 
         childCount++;
@@ -25,7 +22,9 @@ void dfs(int cur)
     }
 
     if (childCount == 0)
+    {
         answer++;
+    }
 }
 
 int main()
@@ -33,35 +32,32 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int N;
     cin >> N;
-    int tem;
-    for (int i = 1; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        cin >> tem;
-        if (tem == -1)
+        int parent;
+        cin >> parent;
+        if (parent == -1)
         {
-            cin >> tem;
+            root = i;
         }
-        tree[tem].push_back(i);
-        tree[i].push_back(tem);
+        else
+        {
+            tree[parent].push_back(i);
+        }
     }
+
     cin >> del;
-    for (int i = 0; i < tree[del].size(); i++)
+
+    if (del == root)
     {
-        tem = tree[del].back();
-        for (int j = 0; j < N; j++)
-        {
-            if (j == del)
-            {
-                continue;
-            }
-            tree[j].erase(remove(tree[j].begin(), tree[j].end(), del), tree[j].end());
-        }
-        tree[del].pop_back();
+        cout << 0 << "\n";
     }
-    dfs(0);
-    cout << answer;
+    else
+    {
+        dfs(root);
+        cout << answer << "\n";
+    }
 
     return 0;
 }
